e80ba16bf9c89afe3c532cc447f08b34103f6fd2
Nagasawa Yoshitoki <>
2019-10-15 22:04:43 +0800

    1015 kadai

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+//jQueryのみ
+// $(function() {
+// });
+
+$("#btn1").on("click",function(){
+    $(".img").fadeIn(1000);
+  });
+
+  $("#btn2").on("click",function(){
+    $(".img").fadeOut(1000);
+  });
